98f0af285cf7c4b9aa8103e639ff30edc25fe5fb
Kongfujonstew <jonstew@gmail.com>
2017-05-03 17:07:01 -0700

    map on index.html and map component - waldo image

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -5,10 +5,11 @@ import {render} from 'react-dom';
 export class Map extends React.Component {
   render () {
     return (
-      <iframe src=`//www.google.com/maps/embed/v1/place?q=Harrods,Brompton%20Rd,%20UK
-            &zoom=17
-            &key=YOUR_API_KEY`> //We need this!
-        </iframe>
+      <div id="map">
+ 
+       </div>  
+
+
     )
   }
 }
