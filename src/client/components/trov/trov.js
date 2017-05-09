bcedafc164b7dd43d9ea0ddd16098f1bd2d283ae
ashwindollar <ashwinkn@gmail.com>
2017-05-09 16:09:57 -0700

    Challenges render based on progress

--- src/client/components/trov/trov.js
+++ src/client/components/trov/trov.js
@@ -6,12 +6,25 @@ import Quest from './quest.js';
 export default class Trov extends React.Component {
   constructor () {
     super();
+    this.state = {
+      progress: 4,
+    }
   }
+  renderChallenges() {
+    var toRender = [];
+    for (var counter = 0; counter < this.state.progress; counter++) {
+      toRender.push(<Quest challenge={this.props.challenges[counter]} key={counter}/>);
+    }
+    return toRender;
+  }
+
   render () {
     return (
     <div id="trov">
       <h2>Mission Meltdown</h2>
-      <Quest />
+      <ol className="quest">
+        {this.renderChallenges()}
+      </ol>
     </div>
     )
   }
