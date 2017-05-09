383e20c2f89f2f97bd1d06f4e91a50136eb21b4e
Brandon <blbrown13@gmail.com>
2017-05-08 21:48:44 -0700

    Refactor index.js and main.js to properly render and remove errors

--- src/client/components/visitormain.js
+++ src/client/components/visitormain.js
@@ -4,10 +4,8 @@ import Troves from './trov/troves.js';
 import TrovMain from './trov/trovmain.js';
 import Header from './header.js';
 
-// <<<<<<< HEAD
-// >>>>>>> 069d935a8b0a4b81d2903eca783ee94e8ec877a1
 
-export default class VisitorMain extends React.Component {
+export class VisitorMain extends React.Component {
   render () {
     return (
     <div id="visitormain" className="jumbotron text-center">
