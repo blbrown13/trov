7d4efb710afe4a86df5f004f8c0ac4057c5a43d6
ashwindollar <ashwinkn@gmail.com>
2017-05-09 14:37:21 -0700

    Implemented conditional rendering and refactored login and logout

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -3,11 +3,23 @@ import render from 'react-dom';
 import Profile from './profile.js'
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({username: newProps.username});
+    console.log(this.state.username);
+  }
+
   render () {
     return (
     <div id="header">
      <img src="../images/trov_logo.png" />
-     <Profile name={"Jake"} />
+     <Profile name={this.state.username} />
     </div>
     )
   }
--- src/client/components/main.js
+++ src/client/components/main.js
@@ -17,7 +17,11 @@ class Main extends React.Component {
       currentTrov: null,
       currentChallengeNum: 0
     }
-    console.log('Test Trov Data: ', testTrov);
+    console.log("Main Logged In? " + this.props.loggedIn);
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({isLoggedIn: newProps.loggedIn});
   }
 
   componentWillMount() {
@@ -29,40 +33,25 @@ class Main extends React.Component {
     });
   }
 
-  handleLogIn () {
-    console.log('handleLogIn');
-    //will need to check if there is a current session, if so:
-    this.setState({
-      isLoggedIn: true
-    })
-  }
+  // handleSelectTrov () {
+  //   console.log('handleSelectTrov');
+  //   if (this.state.isLoggedIn) {
+  //     this.setState({
+  //       isOnTrovNow: true,
+  //       currentTrov: testTrov //change me when we can select new trovs!!!!
+  //     })
+  //   }
+  // }
 
-  handleLogOut () {
-    console.log('handleLogOut');
-    this.setState({
-      isLoggedIn: false
-    })
-  }
-
-  handleSelectTrov () {
-    console.log('handleSelectTrov');
-    if (this.state.isLoggedIn) {
-      this.setState({
-        isOnTrovNow: true,
-        currentTrov: testTrov //change me when we can select new trovs!!!!
-      })
-    }
-  }
-
-  handleCompleteChallenge () {
-    console.log('handleCompleteChallenge');
-    if (this.state.currentTrov && this.state.isLoggedIn) {
-      var newCurrentChallengeNum = this.state.currentChallengeNum+1;
-      this.setState({
-        currentChallengeNum: newCurrentChallengeNum
-      })
-    }
-  }
+  // handleCompleteChallenge () {
+  //   console.log('handleCompleteChallenge');
+  //   if (this.state.currentTrov && this.state.isLoggedIn) {
+  //     var newCurrentChallengeNum = this.state.currentChallengeNum+1;
+  //     this.setState({
+  //       currentChallengeNum: newCurrentChallengeNum
+  //     })
+  //   }
+  // }
 
   handleCompleteTrov () {
     console.log('handleCompleteTrov');
@@ -72,55 +61,63 @@ class Main extends React.Component {
       currentChallengeNum: 0
     })
   }
-
-
   handleCheckLocButtonClick () {
     //checks if window.loc is acceptably close to this.state.currentTrov coordinates
     //gets passed down to trovmain
   }
 
-  render() {
-    // conditional renders here . . . ie if statements, will check isLoggedIn and isOnTrovNow
-
-    // TEST AREA: delete me if not needed
-
-    return (
-
-      <div id="trovmain">
-        <h1>test buttons</h1>
-        <div>currentTrov: {this.state.currentTrov ? this.state.currentTrov.name : 'none'} </div>
-        <div>currentChallengeNum: {this.state.currentChallengeNum}</div>
-        <button onClick={this.handleLogIn.bind(this)}>LogIn</button>
-        <button onClick={this.handleLogOut.bind(this)}>LogOut</button>
-        <button onClick={this.handleSelectTrov.bind(this)}>Select Trov (testTrov)</button>
-        <button onClick={this.handleCompleteChallenge.bind(this)}>Complete Challenge</button>
-        <button onClick={this.handleCompleteTrov.bind(this)}>Complete Trov</button>
-        <div><Map /></div>
-        <div>[GoogleMap goes here]</div>
-        <div id='visitor-main'>{this._renderVisitorMain()}</div>
-        <div id='user'>{this._renderUser()}</div>
-      </div>
-    )
-  }
-
-  _renderVisitorMain () {
+  renderMain() {
     if (!this.state.isLoggedIn) {
       return <VisitorMain />
-    } else {
-      return null;
     }
-  }
-
-  _renderUser () {
-    if (!this.state.isOnTrovNow && this.state.isLoggedIn) {
-      return <UserNoTrovMain />
-    } else if (this.state.isOnTrovNow && this.state.isLoggedIn) {
+    else {
       return <TrovMain />
     }
   }
-  // <VisitorMain/>
-  // )
+
+  render() {
+    // conditional renders here . . . ie if statements, will check isLoggedIn and isOnTrovNow
+    return (
+      this.renderMain()
+    )
+  }
+    // TEST AREA: delete me if not needed
+
+  //   return (
+  //
+  //     <div id="trovmain">
+  //       <h1>test buttons</h1>
+  //       <div>currentTrov: {this.state.currentTrov ? this.state.currentTrov.name : 'none'} </div>
+  //       <div>currentChallengeNum: {this.state.currentChallengeNum}</div>
+  //       <button onClick={this.handleSelectTrov.bind(this)}>Select Trov (testTrov)</button>
+  //       <button onClick={this.handleCompleteChallenge.bind(this)}>Complete Challenge</button>
+  //       <button onClick={this.handleCompleteTrov.bind(this)}>Complete Trov</button>
+  //       <div><Map /></div>
+  //       <div>[GoogleMap goes here]</div>
+  //       <div id='visitor-main'>{this._renderVisitorMain()}</div>
+  //       <div id='user'>{this._renderUser()}</div>
+  //     </div>
+  //   )
   // }
+  //
+  // _renderVisitorMain () {
+  //   if (!this.state.isLoggedIn) {
+  //     return <VisitorMain />
+  //   } else {
+  //     return null;
+  //   }
+  // }
+  //
+  // _renderUser () {
+  //   if (!this.state.isOnTrovNow && this.state.isLoggedIn) {
+  //     return <UserNoTrovMain />
+  //   } else if (this.state.isOnTrovNow && this.state.isLoggedIn) {
+  //     return <TrovMain />
+  //   }
+  // }
+  // // <VisitorMain/>
+  // // )
+  // // }
 
 }
 
