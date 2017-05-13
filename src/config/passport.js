8d0327a2aba8f59a10be14ad5c0bc0797fb0a5ed
Brandon <blbrown13@gmail.com>
2017-05-12 22:42:39 -0700

    Update async method to parse FB req data, and call DB query method to find and/or save user into DB

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,8 +1,13 @@
 var LocalStrategy    = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
+var configAuth = require('./auth');
+var db = require('../server/db.js');
+var express = require('express');
+var server = express.Router();
+
 // load up the user model
 // var User       = require('../server/db.js'); // *NEED TO CHANGE FOR SQL DB
-var configAuth = require('./auth');
+
 
 // var FB = require('fb');
 // fb = new FB.Facebook();
@@ -28,7 +33,8 @@ module.exports = function(passport) {
   passport.use(new FacebookStrategy({
     clientID        : configAuth.facebookAuth.clientID,
     clientSecret    : configAuth.facebookAuth.clientSecret,
-    callbackURL     : configAuth.facebookAuth.callbackURL
+    callbackURL     : configAuth.facebookAuth.callbackURL,
+    profileFields   : ['id', 'emails', 'name', 'displayName']
   },
   // facebook will send back the token and profile
   function(token, refreshToken, profile, done) {
@@ -36,10 +42,21 @@ module.exports = function(passport) {
     console.log('\n[PROFILE]: ', profile);
     console.log('\n');
     // checkToken();
-    return done();
+    // return done();
 
     // asynchronous
-    // process.nextTick(function() {
+    process.nextTick(function() {
+      var newUser = profile.displayName;
+      var facebookId = profile.id;
+      var email = profile.emails[0].value;
+
+      console.log('*Displaying the following info...\n');
+      console.log(`username: ${newUser}`);
+      console.log(`facebookId: ${facebookId}`);
+      console.log(`email: ${email}`);
+
+      doDBQuery(newUser, facebookId, email);
+
     //   find the user in the database based on their facebook id
     //
     //     // if the user is found, then log them in
@@ -57,6 +74,29 @@ module.exports = function(passport) {
     //       });
     //     }
     //   });
-    // });
+      return done();
+    });
   }));
+
+  doDBQuery = function(newUser, facebookId, email){
+    db.connection.query(`use trov`);
+    db.connection.query(`SELECT * FROM users WHERE username = "${newUser}";`,
+      function(error, result) {
+        if(error) {
+          console.log("Error querying")
+        } else {
+          if (result.length === 0) {
+            // username doesn't exist
+            console.log("User doesn't exist!")
+            db.connection.query(`use trov`);
+            db.connection.query(`INSERT INTO users (username, facebookId, email, isLoggedIn) VALUES ("${newUser}", "${facebookId}", "${email}", true)`);
+          } else {
+            db.connection.query(`INSERT INTO users (isLoggedIn) VALUES (true)`);
+            console.log("\nUser exists!")
+            console.log("User is logged-in!\n");
+          }
+        }
+      }
+    );
+  };
 };
