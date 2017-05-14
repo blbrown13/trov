32aa98f47b6e829d82e6085cf45c279be2061cb1
Kongfujonstew <jonstew@gmail.com>
2017-05-13 17:37:18 -0700

    add banner and new login button - styles still needed

--- src/client/components/visitormain.js
+++ src/client/components/visitormain.js
@@ -10,12 +10,13 @@ import Auth from './auth.js';
 export class VisitorMain extends React.Component {
   render () {
     return (
-    <div id="visitormain">
-      <h1>Go on a fun adventure today</h1>
-      <h2>Set up a Trov account</h2>
-      <h2>Join a Trov</h2>
-      <h2>Explore new places</h2>
-      <h2>Hang out with friends</h2>
+    <div>
+      <div id="visitormain">
+        <h1>Find YOUR Trov today</h1>
+      </div>
+      <div id="newloginbutton">
+        <div>Begin your Trov geoadventure now</div>
+      </div>
     </div>
     )
   }
